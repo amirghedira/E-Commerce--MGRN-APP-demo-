b0c67f3e9ebc964ef8a85a9645f1e2194f263cf8
amirghedira <amirghedira06@gmail.com>
2020-07-22 23:56:39 +0100

    deploy config

--- app.js
+++ app.js
@@ -17,9 +17,17 @@ mongoose.connect("mongodb+srv://amirghedirq:RVaeoXZ1Lrk1U9s2@cluster0-bjmuu.mong
 })
 
 app.use(cors())
-app.use(function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    //
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    })
+}
+//build mode
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
+})
 app.use(checkAuth)
 app.use('/graphql', expressGraphql.graphqlHTTP({
     graphiql: true,
